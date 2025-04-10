2acb7b07c81ab49a7e6c9fdbfc145bd849d62d07
Nils <nils@trilobyte.de>
2025-04-10 18:13:50 +0200

    error for version mismatch between rustc 1.81.0 and pq-sys

--- k8security-dashboard/backend/src/main.rs
+++ k8security-dashboard/backend/src/main.rs
@@ -1,4 +1,9 @@
+use backend::establish_connection;
+
+mod schema;
+mod models;
 
 fn main() {
     println!("Hello, world!");
+    let connection = establish_connection();
 }
--- k8security-dashboard/backend/src/models.rs
+++ k8security-dashboard/backend/src/models.rs
@@ -1,15 +1,31 @@
 use diesel::prelude::*;
 use serde::{Deserialize, Serialize};
 
-#[derive(Queryable, Serialize)]
+
+#[derive(Queryable, Selectable)]
+#[diesel(table_name = crate::schema::vulnerability)]
 pub struct Vulnerability{
-
+    pub id: i32,
+    pub vuln_id : i32,
+    pub pkg_name: String,
+    pub installed_version: String,
+    pub severity: String, 
 }
 
-#[derive(Queryable, Insertable, Serialize, Deserialize)]
-#[table_name = "vulnerabilities"]
+#[derive(Queryable, Insertable)]
+#[diesel(table_name = crate::schema::vulnerability)]
 pub struct NewVulnerability{
-
+    pub vuln_id : i32,
+    pub pkg_name: String,
+    pub installed_version: String,
+    pub severity: String, 
 }
 
-
+#[derive(AsChangeset)]
+#[diesel(table_name = crate::schema::vulnerability)]
+pub struct UpdateVulnerability{
+    pub vuln_id : Option<i32>,
+    pub pkg_name: Option<String>,
+    pub installed_version: Option<String>,
+    pub severity: Option<String>, 
+}
