1c2ddea9d621618073f144ac7165a121004c6528
debian <n.rudolphi11@gmail.com>
2025-05-27 21:46:34 +0200

    dummy änderung

--- k8security-dashboard/frontend/src/components/EmailBar.jsx
+++ k8security-dashboard/frontend/src/components/EmailBar.jsx
@@ -7,7 +7,7 @@ export default function EmailBar() {
     const [emails, setEmails] = useState([])
     const [newEmail, setNewEmail] = useState('')
     
-    // Änderung
+    // Änderung 2
     useEffect(() => {
         getReceiverEmails().then(setEmails)
     }, [])
