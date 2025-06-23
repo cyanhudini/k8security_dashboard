013a9fab9564c080d7c27d3d377249c3d1e650d6
Nils <nils@trilobyte.de>
2025-06-23 13:45:51 +0200

    adding smtp_server to repo

---
+++ k8security-dashboard/smtp_server/email_sender.py
@@ -0,0 +1,56 @@
+import smtplib
+import psycopg2
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+def send_email(report, receiver_emails):
+    sender_email = "nils.rudolphi@trilobyte.berlin"
+    subject = "High Severity Vulnerabilities im Kubernetes CLuster"
+
+    msg = MIMEMultipart()
+    msg['From'] = sender_email
+    msg['To'] = ", ".join(receiver_emails)
+    msg['Subject'] = subject
+
+    body = "Im Anhang findest du eine Zusammenfassung der durch Trivy gefunden Vulnerabilities mit einer Schwere höher HIGH."
+    msg.attach(MIMEText(body, 'plain'))
+
+    attachment = MIMEText(report, 'plain')
+    attachment.add_header('Content-Disposition', 'attachment', filename="report.json")
+    msg.attach(attachment)
+    # env
+    server = smtplib.SMTP_SSL('mail.trilobyte.berlin')
+    server.login("nrudolphi", "EndlessInfinite1134")
+    server.sendmail(sender_email, receiver_emails, msg.as_string())
+    server.quit()
+
+def get_active_emails():
+    conn = psycopg2.connect(
+        dbname="vulndb",
+        user="admin",
+        password="123456789",
+        host="localhost",
+        port="5432"
+)
+    cur = conn.cursor()
+    cur.execute("SELECT email_adress FROM emails WHERE receiving = true")
+    emails = [row[0] for row in cur.fetchall()]
+    cur.close()
+    conn.close()
+    return emails
+
+def main():
+    with open("report.json", "r") as f:
+        report_content = f.read()
+    if report_content:
+        receivers = get_active_emails()
+        if receivers:
+            send_email(report_content, receivers)
+        else:
+            print("No active emails found.")
+    else:
+        
+        print("Keine zu sendende Datei gefunden")
+
+if __name__ == "__main__":
+    main()
---
+++ k8security-dashboard/smtp_server/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+while true; do
+    
+
+  if [ $? -eq 0 ]; then
+     
+      python /email_sender.py
+  else
+      echo "Trivy scan failed"
+  fi
+
+  sleep 10
+done
